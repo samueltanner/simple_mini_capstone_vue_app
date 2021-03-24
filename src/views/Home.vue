0b5167655524e356e9f3f675c0505b42497aa74a
samueltanner <samuel.m.s.tanner@gmail.com>
2021-03-24 13:38:29 -0700

    adds ability to connect to backend

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <div v-for="product in products" v-bind:key="product.id">
+        <h2>{{ product.name }}</h2>
+        <img v-bind:src="product.image_url" v-bind:alt="product.name">
+        <p class="price">${{ product.price }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+.price {
+  margin-top: 5px;
+  margin-bottom: 50px;
+}
+
+img {
+  width: 300px;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios"; //this connects to rails
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/api/products").then((response) => {
+        this.products = response.data;
+        console.log(response.data);
+      });
+    },
   },
 };
 </script>
